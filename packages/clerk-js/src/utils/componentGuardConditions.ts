4ef0fb7b75f0a5abf5cc41182b08c2613f7828e5
George Desipris <george.desipris.56@gmail.com>
2023-01-26 10:46:31 +0200

    chore(clerk-js): Rename clerkRequirements to componentGuardConditions

--- packages/clerk-js/src/utils/componentGuardConditions.ts
+++ packages/clerk-js/src/utils/componentGuardConditions.ts
@@ -1,22 +1,22 @@
 import { Clerk, ClerkOptions, EnvironmentResource } from '@clerk/types';
 
-export type ClerkRequirement = (
+export type ComponentGuardCondition = (
   clerk: Clerk,
   environment?: EnvironmentResource | null,
   options?: ClerkOptions,
 ) => boolean;
 
-export const sessionExistsAndSingleSessionModeEnabled: ClerkRequirement = (
+export const sessionExistsAndSingleSessionModeEnabled: ComponentGuardCondition = (
   clerk: Clerk,
   environment?: EnvironmentResource | null,
 ) => {
   return !!(clerk.session && environment?.authConfig.singleSessionMode);
 };
 
-export const noUserExists: ClerkRequirement = (clerk: Clerk) => {
+export const noUserExists: ComponentGuardCondition = (clerk: Clerk) => {
   return !clerk.user;
 };
 
-export const noOrganizationExists: ClerkRequirement = (clerk: Clerk) => {
+export const noOrganizationExists: ComponentGuardCondition = (clerk: Clerk) => {
   return !clerk.organization;
 };
